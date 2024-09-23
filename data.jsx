46cc0c50afcd462e20b7b3157c5d16fe08186edf
kaleycode <readingkale@gmail.com>
2024-09-23 09:18:56 -0700

    Add files via upload

---
+++ data.jsx
@@ -0,0 +1,43 @@
+export const data = [
+    {
+        question: "In what year did Doctor Who first start airing?",
+        option1: "1960",
+        option2: "1954",
+        option3: "1969",
+        option4: "1963",
+        ans: 4,
+    },
+    {
+        question: "What is the name of the first episode to ever introduce the Daleks?",
+        option1: "The Daleks",
+        option2: "Dalek",
+        option3: "The Dalek Invasion of Earth",
+        option4: "Genesis of the Daleks",
+        ans: 1,
+    },
+    {
+        question: "What is the Brigadier's full name?",
+        option1: "Fred Gorgan Lethbridge-Stewart",
+        option2: "Alistair Gordon Lethbridge-Stewart",
+        option3: "Gregory Von Gordon Lethbridge-Stewart",
+        option4: "Alexander Gorgon Lethbridge-Stewart",
+        ans: 2,
+    },
+    {
+        question: "What is the creature in Doctor Who that can send you back in time?",
+        option1: "The Gorgon",
+        option2: "The Weeping Angels",
+        option3: "The Cybermen",
+        option4: "The Slitheen",
+        ans: 2,
+    },
+    {
+        question: "What planet do the Daleks originate from?",
+        option1: "Gallifrey",
+        option2: "Earth",
+        option3: "Skaro",
+        option4: "Blathereen",
+        ans: 3,
+    },
+
+];
